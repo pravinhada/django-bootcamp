b16fe97f5625681069075e1fb30f98ddcfaa6648
Prabin Hada <phada@Prabins-MacBook-Pro.local>
2021-12-17 23:05:05 -0500

    adding more class based view logic

--- class_based_view/basic_app/views.py
+++ class_based_view/basic_app/views.py
@@ -1,10 +1,14 @@
+from django.db import models
+from django.forms.models import fields_for_model
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from basic_app.models import Student, School
+from django.urls import reverse_lazy
 
 
 # Create your views here.
 class IndexView(TemplateView):
-    template_name = 'basic_app/index.html'
+    template_name = 'index.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -12,3 +16,27 @@ class IndexView(TemplateView):
         return context
     
 
+class SchoolListView(ListView):
+    model = School
+    context_object_name = 'schools'
+    
+    
+class SchoolDetailView(DetailView):
+    model = School
+    template_name = 'basic_app/school_detail.html'
+    context_object_name = 'school_detail'
+    
+
+class SchoolCreateView(CreateView):
+    fields = ('name', 'principal', 'location')
+    model = School
+    
+
+class SchoolUpdateView(UpdateView):
+    model = School
+    fields = ('name', 'principal')
+    
+
+class SchoolDeleteView(DeleteView):
+    model = School
+    success_url = reverse_lazy('basic_app:list')
