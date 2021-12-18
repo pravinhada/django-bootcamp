734c9a23fa774d0c7b05afcbb974e58f9ddd9972
Prabin Hada <phada@Prabins-MacBook-Pro.local>
2021-12-18 13:27:26 -0500

    adding urls and views

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,8 +1,12 @@
 from os import name
-from django.urls import path
+from django.urls import path, re_path
 from blog import views
 
 
 urlpatterns = [
-    path('about', views.AboutView.as_view(), name='about')
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('about', views.AboutView.as_view(), name='about'),
+    re_path('post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new', views.CreatePostView.as_view(), name='post_new'),
+    re_path('post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
 ]
