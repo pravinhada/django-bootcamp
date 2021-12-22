7018566b07a05038bb66adc45c99e46cdf73f093
phada <phada@globallink.com>
2021-12-22 13:40:11 -0500

    adding viewset

--- profiles_project/profiles_api/urls.py
+++ profiles_project/profiles_api/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-from profiles_api.views import HelloApiView
+from django.urls import path, include
+from rest_framework import routers
+from profiles_api.views import HelloApiView, HelloViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('hello-viewset', HelloViewSet, basename='hello-viewset')
 
 
 urlpatterns = [
     path('hello/', HelloApiView.as_view(), name='hellow_view'),
+    path('', include(router.urls)),
 ]
